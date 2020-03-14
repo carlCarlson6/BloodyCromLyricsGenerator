741a673a291d990533619bbfa0537fc3ab24c689
carlCarlson6 <bullet.carlos@gmail.com>
2020-03-14 23:20:24 +0100

    working on batch manager

--- Core/BatchManager.py
+++ Core/BatchManager.py
@@ -1,6 +1,13 @@
+import numpy
+
 class BatchManager:
     def GetBatches(self, Data, BatchSize, SequenceLength):
         totalBatchSize = BatchSize*SequenceLength
         numberBatches = len(Data)//totalBatchSize
 
-        
+        Data = Data[:numberBatches * totalBatchSize]
+        Data = Data.reshape((BatchSize, -1))
+
+        for n in range(0, Data.shape[1], SequenceLength):
+            x = Data[:, n:+SequenceLength]
+            y = numpy.zeros_like(x)
