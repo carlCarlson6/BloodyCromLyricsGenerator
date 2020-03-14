a01722946512b925c05d8e75047bd30a1dccd652
carlCarlson6 <bullet.carlos@gmail.com>
2020-03-14 22:16:47 +0100

    .

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import common
 import NN
+import torch
 
 dataLoader = common.DataLoader(TextFileName='Data\\BloodyCromLyrics.txt')
 
 tokenizer = NN.Tokenizer()
 encodedText = tokenizer.EncodeText(Text=dataLoader.Text, Char2IntDictionary=dataLoader.CharactersDictionary['Char2Int'])
-oneHotEncodedText = tokenizer.OneHotEncode(EncodedText=encodedText)
+oneHotEncodedText = tokenizer.EncodeAsOneHot(EncodedText=encodedText)
 
 k = 0
